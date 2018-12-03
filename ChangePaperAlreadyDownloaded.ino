8cc5b20b395b688e695cb0dc75ff93dbc83d6ccf
Elías Orozco <zwitox@gmail.com>
2018-12-03 08:39:24 -0600

    Add files via upload

---
+++ ChangePaperAlreadyDownloaded.ino
@@ -0,0 +1,26 @@
+//This DigiKeyboard Script downloads and sets a wallpaper through the Windows PowerShell. It has been tested successfully
+//on Windows 10 and should probably work with Windows 7/8/8.1 etc.
+
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v Wallpaper /t REG_SZ /d C:\\Temp\\g.bmp /f");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }  
+}
